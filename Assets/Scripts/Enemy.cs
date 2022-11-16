0d5bee6a807a7d9510ee788afe068856a3da877d
Vincent <vincent@reality-academy.fr>
2022-11-16 10:26:10 +0100

    Enemies Acceleration

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,22 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private AnimationCurve curveMouvement;
-    private bool isMoving;
+    [SerializeField] private AnimationCurve m_curveMouvement;
+    private bool m_isMoving;
 
-    [SerializeField] private float durationMovement;
-    private float currentTime;
+    [SerializeField]
+    private float m_initialDurationMovement;
+
+    public float m_InitialDurationMovement => m_initialDurationMovement;
+    
+    private float m_currentDurationMovement;
+    public float m_DurationMovement
+    {
+        get => m_currentDurationMovement;
+        set => m_currentDurationMovement = value;
+    }
+
+
+    private float m_currentTime;
 
     private Vector3 m_initialPos;
     private Vector3 m_targetPos;
 
+    private void Start()
+    {
+        m_currentDurationMovement = m_initialDurationMovement;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (isMoving)
+        if (m_isMoving)
         {
             Move();
         }
@@ -26,20 +44,19 @@ public class Enemy : MonoBehaviour
     {
         m_initialPos = transform.position;
         m_targetPos = transform.position + new Vector3(offsetStepX, offsetStepY);
-        isMoving = true;
-        currentTime = 0;
+        m_isMoving = true;
+        m_currentTime = 0;
     }
 
     public void Move()
     {
-        currentTime += Time.deltaTime / durationMovement;
-        float y = curveMouvement.Evaluate(currentTime);
+        m_currentTime += Time.deltaTime / m_currentDurationMovement;
+        float y = m_curveMouvement.Evaluate(m_currentTime);
         transform.position = Vector3.Lerp(m_initialPos, m_targetPos, y);
-        if (currentTime >= 1)
+        if (m_currentTime >= 1)
         {
-            isMoving = false;
+            m_isMoving = false;
         }
-        
     }
 
     public void Death()
--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -21,10 +22,7 @@ public class EnemyManager : MonoBehaviour
     [SerializeField][Range(0,5)]
     private float m_offsetX, m_offsetY;
 
-    [Header("STEP")] 
-    [SerializeField] 
-    private float m_timeStep;
-    private float m_currentTimeStep;
+    [Header("STEP")]
     [SerializeField]
     private int m_numberStep;
     private int m_currentStep;
@@ -34,26 +32,41 @@ public class EnemyManager : MonoBehaviour
     [SerializeField][Range(-5,0)]
     private float m_offsetStepY;
     [SerializeField] 
-    private float m_delayBetweenEnemy;
+    private float m_initialDelayBetweenRow;
+    private float m_currentDelayBetweenRow;
+
+    [Header("STEP ACCELERATOR")] [SerializeField] [Range(0,1)]
+    private float m_initialDelayBetweenStep;
+    [SerializeField] [Range(0.0001f,1)]
+    private float m_minDelayValue;
+    [SerializeField] 
+    private float m_stepAcceleration;
+    private float m_currentTimeStep;
+    private float m_currentDelayStep;
+    private bool m_canStep;
+
 
     // Start is called before the first frame update
     void Start()
     {
         InitializeWave();
+        m_currentDelayStep = m_initialDelayBetweenStep;
+        m_canStep = true;
+        m_currentDelayBetweenRow = m_initialDelayBetweenRow;
     }
 
     // Update is called once per frame
     void Update()
     {
-    }
-
-    private void FixedUpdate()
-    {
-        m_currentTimeStep += Time.fixedDeltaTime;
-        if (m_currentTimeStep >= m_timeStep)
+        if (m_canStep)
         {
-            m_currentTimeStep -= m_timeStep;
-            StartCoroutine(Step());
+            m_currentTimeStep += Time.deltaTime;
+            if (m_currentTimeStep >= m_currentDelayStep)
+            {
+                m_canStep = false;
+                m_currentTimeStep -= m_currentDelayStep;
+                StartCoroutine(Step());
+            }
         }
     }
 
@@ -73,8 +86,10 @@ public class EnemyManager : MonoBehaviour
                 m_enemies[i, j] = l_enemy;
             }
         }
+        
     }
 
+    #region Step functions
     private IEnumerator Step()
     {
         int l_incrementor = (m_isReversed) ? -1 : 1;
@@ -86,16 +101,75 @@ public class EnemyManager : MonoBehaviour
             l_isClampReached = true;
         }
         
-        for (int i = 0; i < m_nbrLines; i++)
+        if (l_isClampReached)
         {
-            for (int j = 0; j < m_nbrColumns; j++)
+            for (int i = 0; i < m_nbrLines; i++)
             {
-                m_enemies[i,j].Step(
-                    (l_isClampReached) ? 0 : m_offsetStepX * l_incrementor,
-                    (l_isClampReached) ? m_offsetStepY : 0
-                    );
+                VerticalStep(i);
+                yield return new WaitForSeconds(m_currentDelayBetweenRow);
             }
-            yield return new WaitForSeconds(m_delayBetweenEnemy);
+        } else {
+            if (l_incrementor > 0)
+            {
+                for (int i = m_nbrColumns - 1; i >= 0; i--)
+                {
+                    HorizontalStep(i, l_incrementor);
+                    yield return new WaitForSeconds(m_currentDelayBetweenRow);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < m_nbrColumns; i++)
+                {
+                    HorizontalStep(i, l_incrementor);
+                    yield return new WaitForSeconds(m_currentDelayBetweenRow);
+                }
+            }
+        }
+        AccelerateStep();
+        m_currentTimeStep = 0;
+        m_canStep = true;
+    }
+
+    private void VerticalStep(int line)
+    {
+        for (int j = 0; j < m_nbrColumns; j++)
+        {
+            m_enemies[line, j].Step(0, m_offsetStepY);
+        }
+    }
+
+    private void HorizontalStep(int column, int p_incrementor)
+    {
+        for (int j = 0; j < m_nbrLines; j++)
+        {
+            m_enemies[j, column].Step(m_offsetStepX * p_incrementor, 0);
+        }
+    }
+    
+    #endregion
+
+    private void AccelerateStep()
+    {
+        if (m_currentDelayStep > m_minDelayValue)
+        {
+            m_currentDelayStep -= (m_stepAcceleration) / 100.0f;
+            if (m_currentDelayStep < m_minDelayValue)
+            {
+                m_currentDelayStep = m_minDelayValue;
+            }
+            
+            float l_factor = m_currentDelayStep / m_initialDelayBetweenStep;
+            for(int i = 0; i < m_nbrLines; i++)
+            {
+                for(int j = 0; j < m_nbrColumns; j++)
+                {
+                    m_enemies[i, j].m_DurationMovement = m_enemies[i,j].m_InitialDurationMovement * l_factor;
+                }
+            }
+
+            m_currentDelayBetweenRow = m_initialDelayBetweenRow * l_factor;
+
         }
     }
 }
