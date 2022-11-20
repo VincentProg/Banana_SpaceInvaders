f1ee7b8572bbeeca2b90d996160a368ef750469e
Vincent <vincentboutin.dev@gmail.com>
2022-11-20 21:24:51 +0100

    Add recoil

--- Assets/Scripts/GameEntity/PlayerShoot.cs
+++ Assets/Scripts/GameEntity/PlayerShoot.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,11 +12,16 @@ public class PlayerShoot : MonoBehaviour
     [SerializeField] private float shootCooldown;
 
     private float _timer;
+    private bool m_isRecoil;
+    [SerializeField] private float m_strengthRecoil;
+    [SerializeField] private AnimationCurve m_recoilCurve;
+    private float m_initialZ;
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_initialZ = transform.position.z;
     }
 
     // Update is called once per frame
@@ -23,6 +29,11 @@ public class PlayerShoot : MonoBehaviour
     {
         if (_timer > 0)
             _timer -= Time.deltaTime;
+
+        if (m_isRecoil)
+        {
+            Recoil();
+        }
     }
 
     public void ShootInput(InputAction.CallbackContext ctx)
@@ -35,7 +46,20 @@ public class PlayerShoot : MonoBehaviour
                 Bullet shootedBullet = Instantiate(bullet, transform.position, Quaternion.Euler(90,0,0));
                 shootedBullet.InitBullet(bulletSpeed);
                 _timer += shootCooldown;
+                m_isRecoil = true;
             }
+        }
+    }
+
+    private void Recoil()
+    {
+        float l_t = (shootCooldown - _timer) / shootCooldown;
+        float l_y = m_recoilCurve.Evaluate(l_t);
+        float l_Zvalue = Mathf.Lerp(m_initialZ, m_initialZ - m_strengthRecoil, l_y);
+        transform.position = new Vector3(transform.position.x, transform.position.y, l_Zvalue);
+        if (l_t > 1)
+        {
+            m_isRecoil = false;
         }
     }
 }
