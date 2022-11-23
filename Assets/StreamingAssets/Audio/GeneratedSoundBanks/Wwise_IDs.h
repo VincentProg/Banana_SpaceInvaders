e7fae01c989816d34c61ef17e45aeaae3cfa9cb3
HubertAlexis <75834156+Framby21@users.noreply.github.com>
2022-11-24 00:12:31 +0100

    add new songs

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,20 +13,37 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_ALIEN_I_SHOOT = 3289687663U;
+        static const AkUniqueID PLAY_ALIEN_R_MORT = 2658175869U;
+        static const AkUniqueID PLAY_ALIEN_R_SHOOT = 566397092U;
+        static const AkUniqueID PLAY_ALIEN_R_TRIPES = 26695346U;
         static const AkUniqueID PLAY_BANANAMUSIC_01_02 = 968691197U;
         static const AkUniqueID PLAY_BASSMUSIC_03 = 2845497272U;
         static const AkUniqueID PLAY_BASSMUSIC_04 = 2845497279U;
         static const AkUniqueID PLAY_BEEPTEST = 3227834082U;
+        static const AkUniqueID PLAY_CHARA_I_SHOOT = 944772603U;
+        static const AkUniqueID PLAY_CHARA_R_DEPLACEMENT = 2448513527U;
+        static const AkUniqueID PLAY_CHARA_R_SHOOT = 126881520U;
+        static const AkUniqueID PLAY_CHARA_R_TAKEDAMAGE = 3975151617U;
         static const AkUniqueID PLAY_COMBO_01 = 1764480534U;
         static const AkUniqueID PLAY_COMBO_02 = 1764480533U;
         static const AkUniqueID PLAY_COMBO_03 = 1764480532U;
         static const AkUniqueID PLAY_COMBO_04 = 1764480531U;
         static const AkUniqueID PLAY_COMBO_05 = 1764480530U;
+        static const AkUniqueID PLAY_DESTROY_THEM = 220599745U;
+        static const AkUniqueID PLAY_KEEP_GOING = 27047026U;
+        static const AkUniqueID PLAY_MAINMENU_MUSIC = 166541076U;
+        static const AkUniqueID PLAY_MORE_MORE = 523954835U;
+        static const AkUniqueID PLAY_NICE = 422082389U;
+        static const AkUniqueID PLAY_PERFECT = 1725302785U;
         static const AkUniqueID PLAY_SCORE_HIT_JAUGE = 2278692287U;
         static const AkUniqueID PLAY_SCORE_HIT_ROND = 3366501106U;
+        static const AkUniqueID STOP_ALIEN_I_SHOOT = 964149281U;
         static const AkUniqueID STOP_BANANAMUSIC_01_02 = 4199445895U;
         static const AkUniqueID STOP_BASSMUSIC_03 = 2968713298U;
         static const AkUniqueID STOP_BASSMUSIC_04 = 2968713301U;
+        static const AkUniqueID STOP_CHARA_I_SHOOT = 2719607225U;
+        static const AkUniqueID STOP_MAINMENU_MUSIC = 4062428950U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS
@@ -44,8 +61,10 @@ namespace AK
     {
         static const AkUniqueID INIT = 1355168291U;
         static const AkUniqueID SB_MUSIC = 779753582U;
+        static const AkUniqueID SB_SFX = 1006919462U;
         static const AkUniqueID SB_TEST = 690783697U;
         static const AkUniqueID SB_UI = 289583631U;
+        static const AkUniqueID SB_VOICES = 3911025208U;
     } // namespace BANKS
 
     namespace BUSSES
@@ -54,7 +73,15 @@ namespace AK
         static const AkUniqueID MUSIC_BUS = 2680856269U;
         static const AkUniqueID SFX_BUS = 213475909U;
         static const AkUniqueID UI_BUS = 3247222208U;
+        static const AkUniqueID VOICES_BUS = 3493323323U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID EFFECT_01 = 2288170604U;
+        static const AkUniqueID EFFECT_02 = 2288170607U;
+        static const AkUniqueID MONSTER = 2376328173U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
